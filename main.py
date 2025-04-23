10928bc267d3dcee11a60fc36bc7ee247b393ca1
unknown <ut986030@gmail.com>
2025-04-23 16:40:30 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,58 @@
+import random 
+def roll_dice():
+    dice_drawing = {
+        1: (
+            "-----",
+            "|   |",
+            "| o |",
+            "|   |",
+            "-----",
+        ),
+        2: (
+            "-----",
+            "|o  |",
+            "|   |",
+            "|  o|",
+            "-----",
+        ),
+        3: (
+            "-----",
+            "|o  |",
+            "| o |",
+            "|  o|",
+            "-----",
+        ),
+        4: (
+            "-----",
+            "|o o|",
+            "|   |",
+            "|o o|",
+            "-----",
+        ),
+        5: (
+            "-----",
+            "|o o|",
+            "| o |",
+            "|o o|",
+            "-----",
+        ),
+        6: (
+            "-----",
+            "|o o|",
+            "|o o|",
+            "|o o|",
+            "-----",
+        ),
+    }
+    roll = input("Roll the dice (yes/no)? ")
+
+    while roll.lower() == "yes".lower():
+        dice1 = random.randint(1, 6)
+        dice2 = random.randint(1, 6)
+        print("dice rolled: {} and {}".format(dice1, dice2))
+        print("\n".join(dice_drawing[dice1]))
+        print("\n".join(dice_drawing[dice2]))
+        
+        roll = input("Roll again (yes/no)? ")
+  
+roll_dice()
